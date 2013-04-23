c82c28b4df520c18f1821bb96671a4e8c4cc54ba
na1s <nasyrov@gmail.com>
2013-04-23 21:47:16 +0500

    added conf for mms 10 gen

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """ Basic todo list using webpy 0.3 """
 import json
+from bson import *
 import web
 import model
 
@@ -8,6 +9,8 @@ import model
 urls = (
     '/', 'Index',
     '/charts', 'Charts',
+    '/conf/v3/(.+)', 'Conf',
+    '/ping/v1/(.+)', 'Ping',
 )
 
 
@@ -21,23 +24,36 @@ class Index:
         todos = model.get_todos()
         return render.index(todos)
 
+
 class Charts:
     def GET(self):
         """ Show page """
         web.header('Content-Type', 'application/json')
         todos = [
-            { "title": '2008', "value": 20 },
-            { "title": '2009', "value": 10 },
-            { "title": '2010', "value": 5 },
-            { "title": '2011', "value": 5 },
-            { "title": '2012', "value": 20 }
+            {"title": '2008', "value": 20},
+            {"title": '2009', "value": 10},
+            {"title": '2010', "value": 5},
+            {"title": '2011', "value": 5},
+            {"title": '2012', "value": 20}
         ]
         return json.dumps(todos)
 
 
+class Conf:
+    def GET(self,id):
+        web.header('Content-Type', 'application/json')
+        todos = {"disableDbstats": False, "hosts": [
+            {"hostname": "127.0.0.1", "hostKey": "127.0.0.1:27017", "munin": True, "getLogs": False,
+             "sslEnabled": False, "uri": "mongodb://127.0.0.1:27017", "id": "3b2432a9a24815be0aaaafbba46617fc",
+             "profiler": True, "port": 27017}]}
+        b = BSON.encode(todos)
+        return b
 
-
-
+class Ping:
+    def POST(self,id):
+        data = web.data()
+        b = BSON.decode(data)
+        return
 app = web.application(urls, globals())
 
 if __name__ == '__main__':
